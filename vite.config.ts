df7744e454e29892b2444a6ed4562c13ea62827a
Jatematik <jatematik@gmail.com>
2026-01-21 14:41:47 +0600

    add: added pwa plugin

--- vite.config.ts
+++ vite.config.ts
@@ -3,16 +3,59 @@ import { fileURLToPath, URL } from 'node:url'
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import vueDevTools from 'vite-plugin-vue-devtools'
+import { VitePWA } from 'vite-plugin-pwa'
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [
     vue(),
+    VitePWA({
+      registerType: 'autoUpdate',
+      manifest: {
+        name: 'Interviews PWA',
+        short_name: 'Interviews',
+        description: 'A PWA built with Vue 3',
+        theme_color: '#ffffff',
+        icons: [
+          {
+            src: 'mask-192.png',
+            sizes: '192x192',
+            type: 'image/png',
+          },
+          {
+            src: 'mask-512.png',
+            sizes: '512x512',
+            type: 'image/png',
+          },
+        ],
+      },
+      workbox: {
+        globPatterns: ['**/*.{js,css,html,ico,png,svg,woff2}'],
+        navigateFallback: 'index.html',
+        runtimeCaching: [
+          {
+            urlPattern: ({ request }) =>
+              request.destination === 'style' ||
+              request.destination === 'script' ||
+              request.destination === 'worker',
+            handler: 'StaleWhileRevalidate',
+            options: {
+              cacheName: 'static-resources',
+              expiration: {
+                maxEntries: 50,
+                maxAgeSeconds: 30 * 24 * 60 * 60,
+              },
+            },
+          },
+        ],
+      },
+      // devOptions: { enabled: true, type: 'module', navigateFallback: 'index.html' },
+    }),
     vueDevTools(),
   ],
   resolve: {
     alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url))
+      '@': fileURLToPath(new URL('./src', import.meta.url)),
     },
   },
 })
